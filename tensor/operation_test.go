c907e9e616a5be157f3cf8f6b08a950873a3463d
yyf-china <373222102@qq.com>
2017-09-18 11:30:08 +0800

    the tensor operation has been tested

--- tensor/operation_test.go
+++ tensor/operation_test.go
@@ -3,6 +3,7 @@ package tensor_test
 import (
 	"testing"
 	"stj/fieldline/tensor"
+
 )
 
 func TestAdd(t *testing.T) {
@@ -17,14 +18,17 @@ func TestSub(t *testing.T) {
 	v := tensor.Sub(tensor.New(1, 2,4), tensor.New(4, 5,6))
 	if !tensor.Equal(v, tensor.New(-3, -3,-2)) {
 		t.Error("Add func has sth wrong")
+
 	}
 }
 
 
 func TestRescale(t *testing.T) {
-	v := tensor.Rescale(tensor.New(1, 2,4), 3)
-	if !tensor.Equal(v, tensor.New(3, 6,12)) {
+	v := tensor.Rescale(tensor.New(1.0, 2.0,4.0), 3.0)
+
+	if !tensor.Equal(v, tensor.New(3.0, 6.0,12.0)) {
 		t.Error("Add func has sth wrong")
 	}
 }
+
 
