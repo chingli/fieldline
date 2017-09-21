f67eab32b359580294820188fb3e40536420f208
yyf-china <373222102@qq.com>
2017-09-21 20:34:33 +0800

    the gemo has been tested

--- geom/geom_test.go
+++ geom/geom_test.go
@@ -1,1 +1,14 @@
-package geom
+package geom_test
+
+import (
+	"stj/fieldline/geom"
+	"testing"
+)
+
+func TestRect(t *testing.T) {
+	xmin, xmax, ymin, ymax := 2.0, 3.0, 4.0, 5.0
+	_, err := geom.NewRect(xmin, xmax, ymin, ymax)
+	if err != nil {
+		t.Error("building rect is failure")
+	}
+}
