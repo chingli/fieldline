ca6c48cf93ac9e3a600eedb9ba7345785e924f2a
Jin Hongwei <jhw@mtkj.com>
2017-09-25 23:08:11 +0800

    改正拼写错误

--- geom/geom_test.go
+++ geom/geom_test.go
@@ -10,10 +10,10 @@ import (
 func TestRect(t *testing.T) {
 	xmin, ymin, xmax, ymax := 2.0, 3.0, 4.0, 5.0
 	r, err := geom.NewRect(xmin, ymin, xmax, ymax)
-	if !num.Equal(r.Area(), 4.0) {
-		t.Error("func Rech.Area wrong")
-	}
 	if err != nil {
 		t.Error("building Rect failure")
+	}
+	if !num.Equal(r.Area(), 4.0) {
+		t.Error("func Rect.Area wrong")
 	}
 }
